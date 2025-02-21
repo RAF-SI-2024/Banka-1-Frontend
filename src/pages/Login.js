ff12303c517f414179a7ebf154c0ff6a47094e4d
Luka Labalo <LabaloLuka>
2025-02-21 14:24:25 +0100

    Kacim pocetno podignut React projekat

---
+++ src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <h1>Welcome to the Login Page!</h1>
+      <p>This is the starting point of your website.</p>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/pages/Registration.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Registration() {
+  return (
+    <div>
+      <h1>Welcome to the Registration Page!</h1>
+      <p>This is the starting point of your website.</p>
+    </div>
+  );
+}
+
+export default Registration;

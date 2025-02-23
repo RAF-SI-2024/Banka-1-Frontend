61db778a06d0aa0d8fbe2cd37e7be548ad82f12b
AleksandarCumic <acumic11622rn@raf.rs>
2025-02-23 20:24:53 +0100

    Napravljen kostur za stranici user portal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserPortalComponent } from './user-portal/user-portal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserPortalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user-portal/user-portal.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Employee {
+  id: number;
+  ime: string;
+  prezime: string;
+  email: string;
+  pozicija: string;
+  broj_telefona: string;
+  aktivan: boolean;
+}
+
+@Component({
+  selector: 'app-user-portal',
+  templateUrl: './user-portal.component.html',
+  styleUrls: ['./user-portal.component.css']
+})
+export class UserPortalComponent implements OnInit {
+  employees: Employee[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchEmployees();
+  }
+
+  fetchEmployees() {
+    this.http.get<Employee[]>('/api/users/employees').subscribe({
+      next: (data) => {
+        this.employees = data;
+      },
+      error: (err) => {
+        console.error('Error fetching employees:', err);
+      }
+    });
+  }
+
+  editEmployee(employee: Employee) {
+    // This function will remain empty for now
+  }
+
+  addEmployee(){
+
+  }
+
+  sledece(){
+
+  }
+}
